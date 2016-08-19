cb9f8b774d5e412d12a3c5b2924f1e06d5f143fa
Zheyu Shen <arsdragonfly@gmail.com>
2016-08-19 21:13:42 +0800

    Update defender control scheme

--- memory.update.js
+++ memory.update.js
@@ -3,7 +3,12 @@ var memoryUpdate = {
     var userdata = {
         '57b4a112fba6693d1d36359f' : 'centralContainer',
         '57b5f7d4335105441c3eb19c' : 'centralContainer',
-        '57b603a63407bb7917f5e6b2' : 'centralContainer'
+        '57b603a63407bb7917f5e6b2' : 'centralContainer',
+        'W39N57': {
+          remoteConstructionRooms: [
+            'W39N58'
+          ]
+        }
     }
     Memory.userdata = userdata
   }
--- role.defender.js
+++ role.defender.js
@@ -5,7 +5,12 @@ var roleDefender = {
     {
       if (creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE)
       {
+        if (Game.flags.overrideWaypoint == undefined) {
         creep.moveTo(targets[0])
+      }
+      else {
+        creep.moveTo(Game.flags.overrideWaypoint)
+      }
       }
     }
     else {
